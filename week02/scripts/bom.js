623d95df2ef0b58ff4b04c3df5b91ba7ffc2100b
biancabooth <bbooth@byupathway.edu>
2025-11-08 06:38:27 -0700

    fixing javascript

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,23 +1,26 @@
+    const input = document.querySelector("#favchap");
+    const button = document.querySelector("button");
+    const list = document.querySelector("#list");
 
 button.addEventListener("click",function(){
     if (input.value.trim() !==""){
-        const input = document.querySelector("#favchap");
-        const button = document.querySelector("button");
-        const list = document.querySelector("#list");
+
         const li =  document.createElement("li");
-        const deleteButton = document.createElement("button");
         li.textContent = input.value;
+        const deleteButton = document.createElement("button");
         deleteButton.textContent = "❌"
+        deleteButton.addEventListener("click",function(){
+        list.removeChild(li);});
         li.append(deleteButton);
         list.append(li);
+        input.value = "";
+        input.focus();}
 
-    }
-    else{return focus()}
+    
+    else{input.focus()}
 });
-deleteButton.addEventListener("click",function(){
-    list.removeChild(li);
-    input.focus();
 
-});
-input.value = "";
-input.focus();
+    
+
+
+
